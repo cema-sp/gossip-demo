403ac50ae52a38249d1ac9de2f96fa11b816ebed
cema-sp <cema.rus@gmail.com>
2015-10-18 15:32:01 +0300

    Changing credentials

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"time"
-
+	"github.com/stefankopieczek/gossip/base"
 	"github.com/stefankopieczek/gossip/log"
+	"time"
 )
 
 var (
 	// Caller parameters
 	caller = &endpoint{
-		displayName: "Ryan",
-		username:    "ryan",
-		host:        "192.168.0.2",
+		displayName: base.String{"Semyon"},
+		username:    base.String{"2233553341@sip2sip.info"},
+		host:        "127.0.0.1",
 		port:        5060,
-		transport:   "UDP",
+		transport:   base.String{"UDP"},
 	}
 
 	// Callee parameters
 	callee = &endpoint{
-		displayName: "Ryan's PC",
-		username:    "stefan",
-		host:        "192.168.0.9",
+		displayName: base.String{"Cema SP"},
+		username:    base.String{"cema_sp"},
+		host:        "sipnet.ru",
 		port:        5060,
-		transport:   "UDP",
+		transport:   base.String{"UDP"},
 	}
 )
 
